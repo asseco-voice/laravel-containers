e9a911abce0a18a1a2efe7f6ab4c21431e4a51eb
Marko Dupor <marko.dupor@gmail.com>
2020-09-14 18:47:38 +0200

    Seed default types change

--- src/Database/Seeds/ContainerSeeder.php
+++ src/Database/Seeds/ContainerSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Voice\Containers\Database\Seeds;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Voice\Containers\App\Container;
 
@@ -11,15 +12,47 @@ class ContainerSeeder extends Seeder
 {
     public function run(): void
     {
-        $data = [
+        $now = Carbon::now();
+
+        Container::query()->updateOrCreate(
             ['name' => 'Default'],
-            ['name' => 'Container 1'],
-            ['name' => 'Container 2'],
-            ['name' => 'Container 3'],
-            ['name' => 'Container 4'],
-            ['name' => 'Container 5'],
+            [
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]);
+
+        $data = [
+            [
+                'name'       => 'Container 1',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'name'       => 'Container 2',
+                'created_at' => $now,
+                'updated_at' => $now,
+
+            ],
+            [
+                'name'       => 'Container 3',
+                'created_at' => $now,
+                'updated_at' => $now,
+
+            ],
+            [
+                'name'       => 'Container 4',
+                'created_at' => $now,
+                'updated_at' => $now,
+
+            ],
+            [
+                'name'       => 'Container 5',
+                'created_at' => $now,
+                'updated_at' => $now,
+
+            ],
         ];
 
-        Container::insert($data);
+        Container::query()->insert($data);
     }
 }
